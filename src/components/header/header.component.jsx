65c55de7d0443d7cd86e0842901f01cc13659489
naseelemak <taseelemak@gmail.com>
2021-02-15 11:12:39 +0800

    Add cartIcon and cartDropdown components, implement Redux code for cart

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,13 +7,13 @@ import { connect } from "react-redux";
 // COMPONENT IMPORTS //
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { Link } from "react-router-dom";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
 // firebase
 import { auth } from "../../firebase/firebase.utils";
 
-const Header = (props) => {
-  // const currentUser = useSelector((state) => state.user.currentUser);
-
+const Header = ({ currentUser, isCartHidden }) => {
   return (
     <div className="header">
       <Link className="logo-container" to="/">
@@ -26,7 +26,7 @@ const Header = (props) => {
         <Link className="option" to="/contact">
           CONTACT
         </Link>
-        {props.currentUser ? (
+        {currentUser ? (
           <div className="option" onClick={() => auth.signOut()}>
             SIGN OUT
           </div>
@@ -36,13 +36,16 @@ const Header = (props) => {
             SIGN IN{" "}
           </Link>
         )}
+        <CartIcon />
       </div>
+      {isCartHidden ? null : <CartDropdown />}
     </div>
   );
 };
 
 const mapStateToProps = (state) => ({
   currentUser: state.user.currentUser,
+  isCartHidden: state.cart.hidden,
 });
 
 export default connect(mapStateToProps)(Header);
